39d42506be197179a236b08ae2e343f7f3cacbde
JaniisKn <Janis_Kneubuehler@sluz.ch>
2019-12-04 11:43:08 +0100

    cboStation is now red if it has Not a Value in it

--- SwissTransportWinApp/Views/DeparturesView.cs
+++ SwissTransportWinApp/Views/DeparturesView.cs
@@ -24,7 +24,8 @@ namespace SwissTransportWinApp
             cboDepartureStation.Items.Clear();
             cboDepartureStation.SelectionStart = cboDepartureStation.Text.Length;
             cboDepartureStation.SelectionLength = 0;
-                cboDepartureStation.DroppedDown = true;
+            cboDepartureStation.DroppedDown = true;
+            cboDepartureStation.BackColor = SystemColors.Window;
                 foreach (SwissTransport.Station station in transport.GetStations(cboDepartureStation.Text).StationList)
                 {
                     if (station.Name != null)
@@ -35,10 +36,22 @@ namespace SwissTransportWinApp
         private void btnSearchDepartures_Click(object sender, EventArgs e)
         {
             lstvDepartures.Items.Clear();
+            if (IsStationGiven()) {            
             foreach (StationBoard stationBoard in transport.GetStationBoard(cboDepartureStation.Text, "").Entries) 
             {
                 lstvDepartures.Items.Add(ShowDepartures(stationBoard));
             }
+            }
+        }
+
+        private bool IsStationGiven()
+        {
+            if (cboDepartureStation.Text.Length <= 0 || cboDepartureStation.Text == null)
+            {
+                cboDepartureStation.BackColor = Color.FromArgb(255, 92, 92);
+                return false;
+            }
+            return true;
         }
 
         private ListViewItem ShowDepartures(StationBoard stationBoard)
